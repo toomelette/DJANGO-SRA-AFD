8cc40e90c9dfd4f28e2d7574b80e47e3cfbcb91c
devbertram <bertram.langruto.services@gmail.com>
2021-07-08 14:08:58 +0800

    Payroll Regular Data Report 11 - Filtered Deduction List, Net Calculations

--- front_end/templates/src/js/components/Payroll/printables/PayrollRegularDataReport.js
+++ front_end/templates/src/js/components/Payroll/printables/PayrollRegularDataReport.js
@@ -49,8 +49,9 @@ class PayrollRegularDataReport extends React.Component {
               let allowance_list = [];
               let deduction_table_column = [];
               let allowance_table_column = [];
+              let deduction_list_filtered = [];
 
-              let food_subsidy = 0;
+              {/* let food_subsidy = 0; */}
 
               let fullname = data.fullname;
               let position = data.position;
@@ -58,12 +59,12 @@ class PayrollRegularDataReport extends React.Component {
               let salary_grade = data.salary_grade;
               let step_increment = data.step_increment;
 
-              let net = data.monthly_salary;
-              let total_deductions = 0;
-              let total_allowance = 0;
+              let net = Number(data.monthly_salary);
+              let first_half_pay = 0;
+              let second_half_pay = 0;
 
 
-              // Set Deductions
+              // Set Deductions list
               data.payrollRegularDataDeduc_payrollRegularData.map(data_deduc => {
                 let mnt_obj = {};
                 if(data.payrollRegularMnt_payrollRegularData){
@@ -76,38 +77,30 @@ class PayrollRegularDataReport extends React.Component {
                       acronym: data_deduc.acronym,
                       amount: Number(mnt_obj.mod_value),
                       priority_seq: mnt_obj.deduc_priority_seq,
+                      is_gsis: mnt_obj.deduc_is_gsis,
                     })
-                    deduction_table_column.push(
-                      <div key={data_deduc.id} style={{ width:"50%" }}>
-                        { data_deduc.acronym } : { numberFormat(mnt_obj.mod_value, 2)}
-                      </div> 
-                    )
                   }else{
                     deduction_list.push({
                       code: data_deduc.code,
                       acronym: data_deduc.acronym,
                       amount: Number(data_deduc.amount),
                       priority_seq: data_deduc.priority_seq,
+                      is_gsis: data_deduc.is_gsis,
                     })
-                    deduction_table_column.push(
-                      <div key={data_deduc.id} style={{ width:"50%" }}>
-                        { data_deduc.acronym } : { numberFormat(data_deduc.amount, 2)}
-                      </div> 
-                    )
                   }
                 }
               })
 
 
-              // Set Allowances
+              // Set Allowances list and allowance table
               data.payrollRegularDataAllow_payrollRegularData.map(data_allow => {
                 let mnt_obj = {};
                 if(data.payrollRegularMnt_payrollRegularData){
                   mnt_obj = data.payrollRegularMnt_payrollRegularData.find(data => data.field === data_allow.code)
-                  if(data_allow.code === "allow9"){
+                  {/* if(data_allow.code === "allow9"){
                     const mnt_obj_food_subsidy = data.payrollRegularMnt_payrollRegularData.find(data => data.field === data_allow.code)
                     food_subsidy = mnt_obj_food_subsidy ? Number(mnt_obj_food_subsidy.mod_value) : Number(data_allow.amount);
-                  }
+                  } */}
                 }
                 if(Number(data_allow.amount) > 0){
                   if(mnt_obj){
@@ -155,17 +148,78 @@ class PayrollRegularDataReport extends React.Component {
                 }
               )
               
-              
-              // calculations
-              allowance_list.map(val_allow => net += val_allow.amount )
 
+              // Add Allowance to net 
+              allowance_list.map(val_allow => {
+                net += Number(val_allow.amount)
+              } )
+              
+              console.log("monthly Salary: " + data.fullname)
+              console.log("monthly Salary: " + data.monthly_salary)
+              console.log("net: " + net)
+              console.log("     ")
+              console.log("     ")
+
+              // Minus Deductions to net and Set filtered deduction list
               deduction_list.map(val_deduc => {
-                if((net - val_deduc.amount) > this.MAX_NET){
-                  net -= val_deduc.amount;
+                if(val_deduc.is_gsis === 0 || val_deduc.is_gsis === null){
+                  if((net - val_deduc.amount) > this.MAX_NET){
+                    net -= val_deduc.amount;
+                    deduction_list_filtered.push({
+                      code: val_deduc.code,
+                      acronym: val_deduc.acronym,
+                      amount: Number(val_deduc.amount),
+                      priority_seq: val_deduc.priority_seq,
+                      is_gsis: val_deduc.deduc_is_gsis,
+                    })
+                  }else{
+                    net = this.MAX_NET;
+                    deduction_list_filtered.push({
+                      code: val_deduc.code,
+                      acronym: val_deduc.acronym,
+                      amount: Number(this.MAX_NET - (net - val_deduc.amount)),
+                      priority_seq: val_deduc.priority_seq,
+                      is_gsis: val_deduc.deduc_is_gsis,
+                    })
+                  }
+                }else{
+                  if((net - val_deduc.amount) > this.MAX_NET){
+                    net -= val_deduc.amount;
+                    deduction_list_filtered.push({
+                      code: val_deduc.code,
+                      acronym: val_deduc.acronym,
+                      amount: Number(val_deduc.amount),
+                      priority_seq: val_deduc.priority_seq,
+                      is_gsis: val_deduc.deduc_is_gsis,
+                    })
+                  }
                 }
               })
-              
 
+
+              // Set deduction table
+              deduction_list_filtered.map(val_fil_deduc => {
+                deduction_table_column.push(
+                  <div key={val_fil_deduc.code} style={{ width:"50%" }}>
+                    { val_fil_deduc.acronym } : { numberFormat(val_fil_deduc.amount, 2)}
+                  </div> 
+                )
+              })
+
+
+              // calculate first half and second half pay
+              let net_excess_dec = net % 1;
+              let net_whole_num = net - net_excess_dec;
+              let net_div_two = net_whole_num / 2;
+
+              if(net_div_two % 1 > 0){
+                first_half_pay = ((net_div_two - (net_div_two % 1)) + 1) + net_excess_dec;
+                second_half_pay = (net_div_two - (net_div_two % 1));
+              }else{
+                first_half_pay = (net_div_two + net_excess_dec);
+                second_half_pay = net_div_two;
+              }
+              
 
               // Set fields that exist in mnt 
               if(data.payrollRegularMnt_payrollRegularData){
@@ -207,8 +261,9 @@ class PayrollRegularDataReport extends React.Component {
 
                   <td className="p-2">
                     <>
-                      <span className="mb-2">15TH: amount</span><br/>
-                      <span className="mb-2">30TH: { numberFormat(net, 2) }</span>
+                      <span className="mb-2">15TH: { numberFormat(first_half_pay, 2) }</span><br/>
+                      <span className="mb-2">30TH: { numberFormat(second_half_pay, 2) }</span><br/>
+                      <span className="mb-2">TOTAL: { numberFormat(net, 2) }</span>
                     </>
                   </td>
 
